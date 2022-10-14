512f708277f62f4d69cd4ae200d4c26b6779a399
Jackson Brewer <jacksonbrew22@gmail.com>
2022-10-14 15:28:07 -0500

    added fizzbuz app

---
+++ app.py
@@ -0,0 +1,20 @@
+#Write a program that prints the numbers from 1 to 100.
+#But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”.
+#For numbers that are multiples of both three and five print “FizzBuzz
+
+def fizzbuzz():
+	global n 
+	n = 1
+	while n < 101: 
+	
+		if((n%3==0) and (n%5==0)):
+			print("FizzBuzz")
+
+		elifgit (n%3==0):
+			print("Fizz")
+
+		else:
+			print(n)
+		n += 1 
+
+fizzbuzz()
