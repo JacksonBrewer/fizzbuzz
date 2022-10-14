0e77ff92fceba7f10992f804fa929cd9e2afd865
Jackson Brewer <jacksonbrew22@gmail.com>
2022-10-14 16:23:09 -0500

    Added the Buzz code, it was forgotten in the first commit

--- app.py
+++ app.py
@@ -6,13 +6,15 @@ def fizzbuzz():
 	global n 
 	n = 1
 	while n < 101: 
-	
+	   
 		if((n%3==0) and (n%5==0)):
 			print("FizzBuzz")
 
 		elif(n%3==0):
 			print("Fizz")
-
+			
+		elif(n%5==0):
+			print("Buzz")
 		else:
 			print(n)
 		n += 1 
